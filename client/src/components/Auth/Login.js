1cd35001ad2f7b5f5c9910b6fbd1a770bddea891
galubezzz <galubezzz@gmail.com>
2021-02-18 21:43:16 -0800

    add Google Auth to Login component

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,17 @@
 import React from "react";
+import {GoogleLogin} from "react-google-login"
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
-
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = googleUser => {
+    console.log(googleUser)
+    const id_token = googleUser.getAuthResponse().id_token
+    console.log(id_token)
+  }
+  return <GoogleLogin 
+  clientId = "47506495137-hmpi0g2l9kavin8p29oqtajuqa0vg00o.apps.googleusercontent.com" 
+  onSuccess={onSuccess}
+  isSignedIn={true}/>;
 };
 
 const styles = {
