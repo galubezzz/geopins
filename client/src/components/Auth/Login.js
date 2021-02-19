d7a69d03f40c2cbb31f389f83d58486b4d9a88d0
galubezzz <galubezzz@gmail.com>
2021-02-18 23:52:04 -0800

    add context and reducer to the application to store user information

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useContext} from "react";
 import {GraphQLClient} from "graphql-request"
 import {GoogleLogin} from "react-google-login"
 import { withStyles } from "@material-ui/core/styles";
-// import Typography from "@material-ui/core/Typography";
+import Context from "../../context";
+import Typography from "@material-ui/core/Typography";
 const ME_QUERY = `{
   me {
 	_id
@@ -12,19 +13,34 @@ const ME_QUERY = `{
 	}
 }`
 const Login = ({ classes }) => {
+  const {dispatch} = useContext(Context)
   const onSuccess = async googleUser => {
-    console.log(googleUser)
-    const idToken = googleUser.getAuthResponse().id_token;
-    const client = new GraphQLClient("http://localhost:4000/graphql", {
+    try{
+      console.log(googleUser)
+      const idToken = googleUser.getAuthResponse().id_token;
+      const client = new GraphQLClient("http://localhost:4000/graphql", {
       headers: {authorization : idToken}
     })
-    const data = await client.request(ME_QUERY)
-    console.log({data})
+      const {me} = await client.request(ME_QUERY)
+      dispatch({type: "LOGIN_USER", payload: me})
+      //console.log({data})
+    }catch (err){
+        onFailure(err)
+    }
   }
-  return <GoogleLogin 
-  clientId = "47506495137-hmpi0g2l9kavin8p29oqtajuqa0vg00o.apps.googleusercontent.com" 
-  onSuccess={onSuccess}
-  isSignedIn={true}/>;
+  const onFailure = err =>{
+    console.error("Error logging in", err)
+  }
+  return (
+    <div className={classes.root}>
+      <GoogleLogin 
+      clientId = "47506495137-hmpi0g2l9kavin8p29oqtajuqa0vg00o.apps.googleusercontent.com" 
+      onSuccess={onSuccess}
+      onFailure={onFailure}
+      theme="dark"
+      isSignedIn={true}/>
+    </div>
+    )
 };
 
 const styles = {
