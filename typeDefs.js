7ea1a7771248f80fa50f1fa9601fbcabd6710dbe
galubezzz <galubezzz@gmail.com>
2021-02-18 18:57:06 -0800

    describe object types for the project

--- typeDefs.js
+++ typeDefs.js
@@ -7,6 +7,25 @@ module.exports = gql`
         email: String
         picture: String
     }
+
+    type Pin {
+        _id: ID,
+        createdAt: String
+        title: String
+        content: String
+        image: String
+        lalitude: Float
+        longitude: Float
+        author: User
+        comments: [Comment]
+    }
+
+    type Comment {
+        text: String
+        createdAt: String
+        author: User
+    }
+    
     type Query {
         me: User
     }
