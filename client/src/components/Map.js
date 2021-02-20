faa4a939d4fbf3d3a4f16e3f5345e7cea1b47f5d
galubezzz <galubezzz@gmail.com>
2021-02-20 15:42:08 -0800

    add blog area for Pin content

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect, useContext } from "react";
 import ReactMapGL, { NavigationControl, Marker } from "react-map-gl"
 import { withStyles } from "@material-ui/core/styles";
 import PinIcon from "../components/PinIcon"
-import { LngLat } from "mapbox-gl";
 import Context from "../context"
+import Blog from "../components/Blog"
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
@@ -80,6 +80,7 @@ const Map = ({ classes }) => {
           </Marker>
         )}
       </ReactMapGL>
+      <Blog/>
     </div>
   )
 };
