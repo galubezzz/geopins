ba62f82df1537ae9b9dfc0377b5b33afcb2044ed
galubezzz <galubezzz@gmail.com>
2021-02-19 22:04:57 -0800

    implement signout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,6 +5,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import MapIcon from "@material-ui/icons/Map";
 import Typography from "@material-ui/core/Typography";
+import Signout from "../components/Auth/Signout"
 
 const Header = ({ classes }) => {
   const {state} = useContext(Context)
@@ -36,7 +37,7 @@ const Header = ({ classes }) => {
               </Typography>
             </div>
           )}
-
+          <Signout/>
         </Toolbar>
       </AppBar>
     </div>  
