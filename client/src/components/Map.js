3440390888961d32bb4708b1d2c460398b3104f9
galubezzz <galubezzz@gmail.com>
2021-02-20 01:00:23 -0800

    add a map to the application

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,29 @@
 import React from "react";
+import ReactMapGL from "react-map-gl"
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
-
+const MAP_TOKEN = "pk.eyJ1IjoiZ2FsdWJlenp6IiwiYSI6ImNrbGRiYTUwZzBzOHYydm1sbml1aDh0bXcifQ.Ee4INWmMI8XuUl6K9P0mJg"
+const viewport = {
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 13
+}
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle = "mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken = {MAP_TOKEN}
+        {...viewport}
+      >
+
+      </ReactMapGL>
+    </div>
+  )
 };
 
 const styles = {
