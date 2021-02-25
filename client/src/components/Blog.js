185724b6f4b3cf0dc5f044801621eea98de3f1b6
galubezzz <galubezzz@gmail.com>
2021-02-24 23:04:40 -0800

    add styled pin content

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -4,16 +4,19 @@ import Context from "../context"
 import NoContent from "./Pin/NoContent"
 import CreatePin from "./Pin/CreatePin"
 import { Paper } from "@material-ui/core";
+import PinContent from "./Pin/PinContent"
 
 const Blog = ({ classes }) => {
   const { state } = useContext(Context)
-  const { draft } = state
+  const { draft, currentPin } = state
   //const {state: {draft}} = useContext(Context)
   let BlogContent;
-  if (!draft) {
+  if (!draft && !currentPin) {
     BlogContent = NoContent
-  } else if (draft) {
+  } else if (draft && !currentPin) {
     BlogContent = CreatePin
+  } else if (!draft && currentPin){
+    BlogContent = PinContent
   }
   return (
     <Paper className={classes.root}>
