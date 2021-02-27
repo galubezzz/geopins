c90d9b74e5eb740b703c9d1941767f8f0a538927
galubezzz <galubezzz@gmail.com>
2021-02-27 00:35:21 -0800

    set subscriptions

--- resolvers.js
+++ resolvers.js
@@ -2,6 +2,8 @@ const {AuthenticationError, PubSub} = require("apollo-server")
 const Pin = require("./models/Pin")
 const pubsub = new PubSub()
 const PIN_ADDED = "PIN_ADDED"
+const PIN_DELETED = "PIN_DELETED"
+const PIN_UPDATED = "PIN_UPDATED"
 const authenticated = next => (root, args, ctx, info) => {
     if (!ctx.currentUser){
         throw new AuthenticationError("You must be logged in");
@@ -28,6 +30,7 @@ module.exports = {
         }),
         deletePin: authenticated (async (root, args, ctx) => {
            const pinDeleted =  await Pin.findOneAndDelete({_id: args.pinId}).exec()
+           pubsub.publish(PIN_DELETED, {pinDeleted})
            return pinDeleted
         }),
         createComment: authenticated(async(root, argx, ctx) => {
@@ -37,12 +40,19 @@ module.exports = {
                 {$push: {comments: newComment }},
                 {new: true}
                 ).populate("author").populate("comments.author")
+                pubsub.publish(PIN_UPDATED, {pinUpdated})
                 return pinUpdated
         })
     },
     Subscription: {
         pinAdded:{
             subscribe: ()=> pubsub.asyncIterator(PIN_ADDED)
+        },
+        pinDeleted:{
+            subscribe: ()=> pubsub.asyncIterator(PIN_DELETED)
+        },
+        pinUpdated:{
+            subscribe: ()=> pubsub.asyncIterator(PIN_UPDATED)
         }
 
     }
