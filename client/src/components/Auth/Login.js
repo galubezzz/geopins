8ee69c7d8d725284733fe62fbe301d9d4de25112
galubezzz <galubezzz@gmail.com>
2021-02-27 00:05:24 -0800

    handle expired auth token errors

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -17,14 +17,15 @@ const Login = ({ classes }) => {
     })
       const {me} = await client.request(ME_QUERY)
       dispatch({type: "LOGIN_USER", payload: me})
-      dispatch({type: "IS_LOGGED_IN", payload: googleUser.isSignedIn()})
+      dispatch({type: "IS_LOGGED_IN", payload: false})
       //console.log({data})
     }catch (err){
         onFailure(err)
     }
   }
   const onFailure = err =>{
-    console.error("Error logging in", err)
+    console.error("Error logging in", err);
+    dispatch({type: "IS_LOGGED_IN", payload: googleUser.isSignedIn()})
   }
   return (
     <div className={classes.root}>
